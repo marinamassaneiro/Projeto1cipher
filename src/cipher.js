116d0670fdec28f15dc3195b46c90c3d46bc04ec
marinamassaneiro <marinamassaneiro@gmail.com>
2022-07-20 17:20:21 -0300

    versãquase completa

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
-const cipher = {
-  // ...
-};
+const cipher = { 
+    encode: function encode (word,offset) {
+        let resultEncode = "";
+        for (let i = 0; i < word.length; i++) {
+            let wordEncode = parseInt (word.charCodeAt(i));
+
+            if (wordEncode >= 65 && wordEncode <= 90) {
+                let charCode = ((((wordEncode - 65) + offset) % 26) + 65);
+                resultEncode = resultEncode + String.fromCharCode(charCode);
+            } else {
+                alert ("Você precisa digitar apenas letras maiúsculas");
+                break;
+            }
+        }
+        return resultEncode + " (" + offset + ")";
+    },
+    decode: function decode (word,offset) {
+        let resultDecode = "";
+        for (let i = 0; i < word.length; i++) {
+            let wordDecode = parseInt (word.charCodeAt(i));
+
+            if (wordDecode >= 65 && wordDecode <= 90) {
+                let charCode = ((((wordDecode - 65) + (26 - offset)) % 26) + 65);
+                resultDecode = resultDecode + String.fromCharCode(charCode);
+            } else {
+                alert ("Você precisa digitar apenas letras maiúsculas");
+                break;
+            }
+        }
+        return resultDecode
+    }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import cipher from './cipher.js';
 
+let buttonEncode = document.querySelector("#buttonEncode");
+buttonEncode.addEventListener ("click", messageEncode);
+
+function messageEncode (e) {
+    e.preventDefault ()
+    let chosenWordEncode = document.querySelector("#wordEncode").value;
+    let chosenNumberEncode = document.querySelector("#numberEncode").value;
+    let numberEncode = parseInt (chosenNumberEncode);
+
+    let encoded = cipher.encode (chosenWordEncode,numberEncode);
+    let resultEncode = document.querySelector("#resultEncode");
+    resultEncode.innerHTML = encoded;
+}
+
+
+let buttonDecode = document.querySelector("#buttonDecode");
+buttonDecode.addEventListener ("click", messageDecode);
+
+function messageDecode (e) {
+    e.preventDefault ()
+    let chosenWordDecode = (document.querySelector("#wordDecode").value);
+    let chosenNumberDecode = (document.querySelector("#numberDecode").value);
+    let numberDecode = parseInt (chosenNumberDecode);
+
+    let decoded = cipher.decode (chosenWordDecode,numberDecode);
+    let resultDecode = document.querySelector("#resultDecode");
+    resultDecode.innerHTML = decoded;
+}
+
 console.log(cipher);
