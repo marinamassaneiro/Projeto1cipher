02f8de684178244e4e5f46966ca851c232840885
marinamassaneiro <marinamassaneiro@gmail.com>
2022-07-22 14:18:17 -0300

    tudo ok com minuscula

--- src/cipher.js
+++ src/cipher.js
@@ -4,17 +4,23 @@ const cipher = {
         for (let i = 0; i < word.length; i++) {
             let wordEncode = parseInt (word.charCodeAt(i));
 
-            if (wordEncode >= 65 && wordEncode <= 90) {
-                let charCode = ((((wordEncode - 65) + offset) % 26) + 65);
-                resultEncode = resultEncode + String.fromCharCode(charCode);
-            
-            } else {
+            if (wordEncode >= 65 && wordEncode <= 122) {
+                let offsetTwo = parseInt (offset / 26);
+                let offsetThree = (offset - (26 * offsetTwo));
+                let charCode = ((((wordEncode - 65) + offsetThree) % 58) + 65);
+                if (charCode >= 91 && charCode <= 96) {
+                    let minCharCode = charCode + 6
+                    resultEncode = resultEncode + String.fromCharCode(minCharCode);
+                } else {
+                     resultEncode = resultEncode + String.fromCharCode(charCode);
+            } } else {
                 alert ("Você precisa digitar apenas letras maiúsculas");
                 break;
             }
         }
         return resultEncode + " (" + offset + ")";
     },
+
     decode: function decode (word,offset) {
         let resultDecode = "";
         for (let i = 0; i < word.length; i++) {
