bcb27dae51d86f5d357040c40455f3a938d45414
marinamassaneiro <marinamassaneiro@gmail.com>
2022-07-22 15:10:31 -0300

    cipher com minuscula separa pra teste

--- src/cipher.js
+++ src/cipher.js
@@ -3,18 +3,19 @@ const cipher = {
         let resultEncode = "";
         for (let i = 0; i < word.length; i++) {
             let wordEncode = parseInt (word.charCodeAt(i));
+            let offsetTwo = parseInt (offset / 26);
+            let offsetThree = (offset - (26 * offsetTwo));
 
-            if (wordEncode >= 65 && wordEncode <= 122) {
-                let offsetTwo = parseInt (offset / 26);
-                let offsetThree = (offset - (26 * offsetTwo));
-                let charCode = ((((wordEncode - 65) + offsetThree) % 58) + 65);
-                if (charCode >= 91 && charCode <= 96) {
-                    let minCharCode = charCode + 6
-                    resultEncode = resultEncode + String.fromCharCode(minCharCode);
-                } else {
-                     resultEncode = resultEncode + String.fromCharCode(charCode);
-            } } else {
-                alert ("Você precisa digitar apenas letras maiúsculas");
+            if (wordEncode >= 65 && wordEncode <= 90) {
+                let charCode = ((((wordEncode - 65) + offsetThree) % 26) + 65);
+                resultEncode = resultEncode + String.fromCharCode(charCode);
+            
+            } else if (wordEncode >= 97 && wordEncode <= 122) {
+                let charCode = ((((wordEncode - 97) + offsetThree) % 26) + 97);
+                resultEncode = resultEncode + String.fromCharCode(charCode);
+             
+            } else {
+                alert ("Você precisa digitar apenas letras minúsculas ou maiúsculas sem acento.");
                 break;
             }
         }
@@ -25,15 +26,19 @@ const cipher = {
         let resultDecode = "";
         for (let i = 0; i < word.length; i++) {
             let wordDecode = parseInt (word.charCodeAt(i));
+            let offsetTwo = parseInt (offset / 26);
+            let offsetThree = (offset - (26 * offsetTwo));
 
             if (wordDecode >= 65 && wordDecode <= 90) {
-                let offsetTwo = parseInt (offset / 26);
-                let offsetThree = (offset - (26 * offsetTwo));
                 let charCode = (((((wordDecode - 65) - offsetThree) + 26) % 26) + 65);
                 resultDecode = resultDecode + String.fromCharCode(charCode);
                 
+            } else if (wordDecode >= 97 && wordDecode <= 122) {
+                let charCode = (((((wordDecode - 97) - offsetThree) + 26) % 26) + 97);
+                resultDecode = resultDecode + String.fromCharCode(charCode);
+            
             } else {
-                alert ("Você precisa digitar apenas letras maiúsculas");
+                alert ("Você precisa digitar apenas letras minúsculas ou maiúsculas sem acento.");
                 break;
             }
         }
